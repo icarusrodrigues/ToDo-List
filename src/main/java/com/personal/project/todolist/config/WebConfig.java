a2a104f57d32f5eee01f26a6fbfda3968222d03a
Icarus Rodrigues <icaros1105@gmail.com>
2024-04-22 22:55:43 -0300

    Adding initial web security config and password encryption logic

---
+++ src/main/java/com/personal/project/todolist/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.personal.project.todolist.config;
+
+import com.personal.project.todolist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/**").permitAll().anyRequest().permitAll();
+            });
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+}
--- src/main/java/com/personal/project/todolist/service/UserService.java
+++ src/main/java/com/personal/project/todolist/service/UserService.java
@@ -5,14 +5,23 @@ import com.personal.project.todolist.mapper.GenericMapper;
 import com.personal.project.todolist.model.User;
 import com.personal.project.todolist.repository.IRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends CrudService<UserDto, User> {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     public UserService(GenericMapper<UserDto, User> mapper, IRepository<User, Long> repository) {
         super(mapper, repository);
     }
 
+    @Override
+    public UserDto create(UserDto dto) {
+        dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+        return super.create(dto);
+    }
 }
